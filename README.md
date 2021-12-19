6b9789177bbc72d9ba70cff2224db543aab3ba1b
Sebastian Hulse <seb.hulse@yahoo.co.uk>
2021-12-19 18:54:14 +0000

    complete overhaul

--- README.md
+++
@@ -1,3 +0,0 @@
-# Instructions
-
-Fill in the christmas.md file with markdown to be compiled into HTML client-side.
---
+++ script.js
@@ -0,0 +1,98 @@
+let tl = gsap.timeline();
+
+tl.from("#i", {
+  opacity: 0,
+  scale: 0,
+  ease: "power4",
+  duration: 1,
+});
+tl.from(
+  "#hb",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.1"
+);
+tl.from(
+  "#text",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.1"
+);
+tl.from(
+  "#surprise",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.1"
+);
+tl.from(
+  "#surprise1",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.1"
+);
+tl.from(
+  "#photo",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.1"
+);
+tl.from(
+  "#seb",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.25"
+);
+tl.from(
+  "#isaac",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.25"
+);
+tl.from(
+  "#ruby",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.25"
+);
+tl.from(
+  "#sign",
+  {
+    opacity: 0,
+    scale: 0,
+    ease: "power4",
+    duration: 1,
+  },
+  "<0.25"
+);
